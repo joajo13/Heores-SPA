a5477fa64f9ec7461502160beef2d2d6bba8029c
juan <juanbejamingiupponi@gmail.com>
2023-09-21 10:24:44 -0300

    New commit for images bug

--- src/Heroes/components/HeroCard.jsx
+++ src/Heroes/components/HeroCard.jsx
@@ -15,7 +15,7 @@ export const HeroCard = ({
   first_appearance,
   characters,
 }) => {
-  const heroImageUrl = `/heroes/${id}.jpg`;
+  const heroImageUrl = `/public/heroes/${id}.jpg`;
 
   return (
     <div className="col animate__animated animate__fadeInRightBig">
